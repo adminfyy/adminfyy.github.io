83fe91e4220127f0e4a7dbb8b9b75ec77c1a0cd1
unknown <fuyy@PCW00373.g-bits.com>
2016-12-14 11:02:00 +0800

    upload dist file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
  const webpack = require('webpack');
 
  module.exports = {
-     entry: '.src/main.js',
+     entry: './src/main.js',
      output: {
          path: './dist',
          filename: 'app.bundle.js',
