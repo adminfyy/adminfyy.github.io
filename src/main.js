3d1c6c5654e09b40a28a6248e20e82c53e67d09d
unknown <fuyy@g-bits.com>
2016-12-14 17:24:26 +0800

    code

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { log } from './log';
+
+var adminlog = log('admin:');
+adminlog('this is the main.js file:');
+adminlog('this is the main.js file:');
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+ const webpack = require('webpack');
+ const htmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+     entry: './src/main.js',
+     output: {
+         path: './dist',
+         filename: `[name].js`,
+     },
+     module: {
+         loaders: [{
+             test: /\.js$/,
+             exclude: /node_modules/,
+             loader: 'babel-loader'
+         },{
+            test: /\.css$/,
+            exclude: /node_modules/,
+            loader: 'css-loader'
+         }]
+     },
+     plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false
+            }
+        }),
+        new htmlWebpackPlugin({
+            title: 'Why To Javascript',
+            template: 'index.html',
+            inject: true
+        })
+     ]
+ }
