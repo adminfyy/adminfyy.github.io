8c278b27ba398e16ce5ca778b3014c8f4bf93000
傅仰乙 <287732502@qq.com>
2016-12-20 23:36:15 +0800

    home page edited

--- src/module/home.vue
+++ src/module/home.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="thumb">
 			__yy JavaScript
+			<p>FrontEnd developed by vue.vue-router.</p>
+			<footer>power by <a href="https://github.com/adminfyy"></a></footer>
 	</div>
 </template>
 <script>
