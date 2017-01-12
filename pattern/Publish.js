9c3d589bad0cf6e5af022c6526ce372236d1c3e3
unknown <fuyy@g-bits.com>
2017-01-12 11:14:46 +0800

    publish implement

---
+++ pattern/Publish.js
@@ -0,0 +1,49 @@
+// Publish/subscribe.js
+
+var pubsub = {};
+
+(function(myObject){
+
+	var topics = {}
+	var subscribeId = 0;
+
+	myObject.subscribe = function(topic, func) {
+		if(!topics[topic]){
+			topics[topic] = []
+		}
+
+		var token = (++subscribeId).toString();
+		topics[topic].push({
+			func: func,
+			token: token
+		})
+
+		return token
+	}
+
+	// unsubscribe single token subscribe
+	myObject.unsubscribe = function(token){
+		for(var t in topics){
+			var currentTopic = topics[t];
+
+			for (var i = 0, j = topics[m].length; i < j; i++) {
+				if(currentTopic[i] === token) {
+					currentTopic.splice(i,1)
+					return token
+				}
+			};
+		}
+	}
+
+	myObject.Publish = function(topic, args){
+		var targetSubscribers = topics[topic] || [];
+
+		for (var i = 0,j = targetSubscribers.length; i < j; i++) {
+			targetSubscribers[i].func(args)
+		};
+
+		return this;
+	}
+
+
+})(pubsub)
