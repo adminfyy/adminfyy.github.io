1259256d979564a453fc78a12613c7d9c71afa62
yangyi fu <287732502@qq.com>
2023-12-01 16:06:07 +0800

    chore: change about page style

--- src/module/about.vue
+++ src/module/about.vue
@@ -1,57 +1,101 @@
 <template>
-	<div class="about">
-    	<h5 class="fl">{{ msg }}</h5>
-    	<div v-for="color in colorful" class="square" v-bind:style="{ 'background': color}"></div>
-	</div>
+  <div class="about">
+    <div class="fl">{{ msg }}</div>
+    <TransitionGroup name="list">
+      <div
+        v-for="color in displayColor"
+        class="square"
+        v-bind:style="{ background: color }"
+        :key="color"
+      >
+      <span>
+        {{ color }}
+      </span>
+      </div>
+    </TransitionGroup>
+  </div>
 </template>
 <script>
 export default {
-  name: 'about',
-  data () {
+  name: "about",
+  data() {
     return {
-      msg: 'About Page'
-    }
+      msg: "霓虹灯 About Page",
+      num: 0,
+    };
   },
   computed: {
-  	colorful () {
-  		const colorArray = [];
-      let color;
-  		for (var i = 0; i < 256; i++) {
-  			color = `rgba(${this.getRandomColor()},${this.getRandomColor()},${this.getRandomColor()}, 0.8)`;
-  			colorArray.push(color);
-  		};
-  		return colorArray;
-  	}
+    colorful() {
+      const colorArray = [];
+      const step = 10;
+      for (var i = 0; i < 255; i+=step) {
+        for (var j = 0; j < 255; j+=step) {
+          for (var k = 0; k < 255; k+=step) {
+            colorArray.push(`rgba(${k}, ${j}, ${i}, 0.8)`);
+          }
+        }
+      }
+      return colorArray;
+    },
+
+
+    displayColor() {
+      return this.colorful.slice(this.num , this.num + 300);
+    },
+  },
+
+  mounted() {
+    setInterval(() => {
+      this.num++
+      this.num%= this.colorful.length
+    }, 32);
   },
 
   methods: {
-    getRandomColor () {
-      return parseInt(Math.random() * 256);
-    }
-  }
-}
+    getRandomColor(base) {
+      return base + parseInt(Math.random() * 256);
+    },
+  },
+};
 </script>
 
 <style>
-.about{
+.about {
   font-size: 0px;
   text-align: left;
-  /*margin: 0px 10rem;*/
-  /*max-width: 50rem;*/
+  margin: 16px 16px;
 }
-.square{
+.square {
   font-size: 0px;
-	width: 50px;
-	height: 50px;
-	display: inline-block;
-	margin: 0px 0px;
-	padding: 0px 0px;
-	background-size: 100% 100%;
+  width: 50px;
+  height: 50px;
+  display: inline-block;
+  margin: 4px 4px;
+  padding: 0px 0px;
+  background-size: 100% 100%;
+  border-radius: 8px;
 }
-.fl{
+.fl {
   position: absolute;
-  color: white;
+  color: black;
+  font-size: 2rem;
+  margin-top: 10rem;
   left: 50%;
-  font-size: 1rem;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+/* 确保将离开的元素从布局流中删除
+  以便能够正确地计算移动的动画。 */
+  .list-leave-active {
+  position: absolute;
 }
 </style>
